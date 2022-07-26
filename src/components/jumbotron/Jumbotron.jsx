c1e592e8d3b6feb99c9315eaebe4037b821aa787
Adilxa <adilhaimerov82@gmail.com>
2022-07-27 02:09:16 +0600

    feat;fix

--- src/components/jumbotron/Jumbotron.jsx
+++ src/components/jumbotron/Jumbotron.jsx
@@ -11,7 +11,7 @@ export default function Jumbotron() {
             <h1>{t("jumbotron.title")}</h1>
             <img
               className={css.fire}
-              src={"/images/footer/Intercomm.svg"}
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/FireIcon.svg/1200px-FireIcon.svg.png"
             />
           </div>
           <p>{t("jumbotron.paragraph")}</p>
