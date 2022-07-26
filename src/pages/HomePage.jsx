db95a6f103cc71ed45bd4fc269b14fea1648662d
Adilxa <adilhaimerov82@gmail.com>
2022-07-27 02:28:23 +0600

    feat/fixed

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,11 @@
 import BottomCompanies from "../Components/bottomCompanies/BottomCompanies";
 import Benefits from "../Components/benefits/Benefits";
-import Jumbotron from "../Components/jumbotron/Jumbotron";
 import Footer from "../Components/footer/Footer";
 import Header from "../Components/header/Header";
 export default function HomePage() {
   return (
     <div>
       <Header />
-      <Jumbotron />
       <Benefits />
       <BottomCompanies />
       <Footer />
