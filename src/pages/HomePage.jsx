2dcfa059942840ae1c5533ebbeae1e0aa28eab36
Adilxa <adilhaimerov82@gmail.com>
2022-08-15 03:31:15 +0600

    feat:ghel

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -11,7 +11,7 @@ export default function HomePage() {
       <Jumbotron />
       <Companies />
       <Benefits />
-      <BottomCompanies /> 
+      <BottomCompanies />
       <Footer />
     </div>
   );
