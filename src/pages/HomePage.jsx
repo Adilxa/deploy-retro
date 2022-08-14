01dc628dd188544cd3a24aa924cafe8f5c063c07
Adilxa <adilhaimerov82@gmail.com>
2022-08-15 02:48:25 +0600

    feat:ghel

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import Companies from "../components/companies/Companies";
-import Jumbotron from "../components/jumbotron/Jumbotron";
 import BottomCompanies from "../components/bottomCompanies/BottomCompanies";
+import Jumbotron from "../components/jumbotron/Jumbotron";
 import Benefits from "../components/benefits/Benefits";
 import Footer from "../components/footer/Footer";
 import Header from "../components/header/header";
