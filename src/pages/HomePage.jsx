26ade698a40a527cd6d79a10284b5ceb80b51fc1
Adilxa <adilhaimerov82@gmail.com>
2022-08-15 03:00:50 +0600

    feat:ghel

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,9 @@
-import Header from "../components/header/Header";
 import Jumbotron from "../components/jumbotron/Jumbotron";
 import Companies from "../components/companies/Companies";
 import Benefits from "../components/benefits/Benefits";
 import BottomCompanies from "../components/bottomCompanies/BottomCompanies";
 import Footer from "../components/footer/Footer";
+import Header from "../components/header/Header";
 export default function HomePage() {
   return (
     <div>
